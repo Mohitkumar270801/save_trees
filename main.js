18d5d3e2122f7fad36c163eadff66668c4c3c358
Mohit kumar <126099991+Mohitkumar270801@users.noreply.github.com>
2023-03-27 02:21:23 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var img = document.getElementById('myImage');
+let currentImgIdx = 0;
+currenttext=0;
+const images = [ 
+    'https://tse4.mm.bing.net/th?id=OIP.x-VO0WWj2Ez5QY4YdZ5-IwHaE8&pid=Api&P=0',
+    'https://tse3.mm.bing.net/th?id=OIP.1BrzXH6DfbfKYRupLTwsUgHaHa&pid=Api&P=0',
+    'https://tse1.explicit.bing.net/th?id=OIP.nJp3sGabRqvIs_Y63P6LKwHaE8&pid=Api&P=0',
+    'https://tse2.mm.bing.net/th?id=OIP.pUdoD-lUhMl2wstHJmSJwgAAAA&pid=Api&P=0',
+    'https://tse4.mm.bing.net/th?id=OIP.UbyzycSwNKaBG5kEuSLqQAHaHa&pid=Api&P=0',
+]; 
+var h1=document.getElementById("h1");
+var array=['Trees purify our air and combat climate change. ...',
+    'Trees provide housing to millions of species that protect us from disease. ...',
+    'Trees cool our streets and cities. ...',
+    'Trees protect against floods and water pollution. ...',
+    'Trees ease the mind during stressful times.'
+]
+function changeImage(){
+    
+    if(currentImgIdx>=images.length){
+        currentImgIdx = 0;
+    } 
+    img.src = images[currentImgIdx];
+    currentImgIdx++;
+    if(currenttext>=array.length){
+        currenttext=0;
+    }
+    var text1= h1.innerHTML=array[currenttext];
+    currenttext++;
+    
+}
